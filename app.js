552ab0a00bcfd6d1d4b9215c5281cc9a0f9c9406
ErzaaGashi <erzzag15@gmail.com>
2023-12-11 22:08:45 +0100

    NodeJs User Management

---
+++ app.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+
+const app = express();
+const port  = 5000 || process.env.PORT;
+
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//Staic files
+app.use(express.static('public'));
+
+//Templating Engine
+app.use(expressLayout);
+app.set('layout','./layouts/main');
+app.set('view engine', 'ejs');
+
+//Home
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(port, ()=>{
+    console.log(`App listing in port ${port}`)
+});
