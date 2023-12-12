b199490ecedcecc08b8b808cbcc8c76b2c765aa7
ErzaaGashi <erzzag15@gmail.com>
2023-12-12 21:05:30 +0100

    Views Created

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ app.use(express.static('public'));
 
 //Templating Engine
 app.use(expressLayout);
-app.set('layout','./layouts/main');
 app.set('view engine', 'ejs');
+app.set('layout', 'layouts/main');
 
 //Home
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.render('index');
 });
 
 app.listen(port, ()=>{
