345a1c9bfa6345e6159376d395304cc7374b3ccb
ErzaaGashi <erzzag15@gmail.com>
2023-12-13 21:49:58 +0100

    Add Customer created

--- server/controllers/customerController.js
+++ server/controllers/customerController.js
@@ -1,7 +1,5 @@
 
 
-
-
 exports.homepage = async (req,res) =>{
  
     const locals ={
@@ -9,4 +7,14 @@ exports.homepage = async (req,res) =>{
         description: 'Free NodeJs User Management System'
     }
     res.render('index', locals);
+}
+
+//Get New Customer 
+exports.addCustomer = async (req,res) =>{
+ 
+    const locals ={
+        title: 'Add New Customer',
+        description: 'NodeJs User Management System'
+    }
+    res.render('customer/add', locals);
 }
--- server/routes/customer.js
+++ server/routes/customer.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const customerController = require('../controllers/customerController');
 
 router.get('/', customerController.homepage)
+router.get('/add', customerController.addCustomer)
+
 
 module.exports=router;
