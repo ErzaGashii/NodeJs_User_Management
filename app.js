b087bf1826c4cf46f721a979ef5bc607b66f7c7b
ErzaaGashi <erzzag15@gmail.com>
2023-12-16 22:01:08 +0100

    Done

--- app.js
+++ app.js
@@ -1,65 +1,59 @@
 require('dotenv').config();
 
-const express = require("express");
-const expressLayout = require("express-ejs-layouts");
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+const methodOverride = require('method-override');
+
+// npm uninstall express-flash-message
 //const { flash } = require('express-flash-message');
-//const flash = require('express-flash-message');
-const flash = require('express-flash');
+
+// npm install connect-flash
+const flash = require('connect-flash');
 
 const session = require('express-session');
 const connectDB = require('./server/config/db');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
 // Connect to Database  
 connectDB();
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method'));
 
-//Staic files
+// Static Files
 app.use(express.static('public'));
 
-//Express Session
+// Express Session
 app.use(
-    session({
-        secret: 'secret',
-        resave: false,
-        saveUninitialized: true,
-        cookie: {
-            maxAge: 1000*60*60*24*7 , //1 week
-        }
-    })
-    )
+  session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+    }
+  })
+);
 
-    //Flash Message
-    app.use(flash({ sessionKeyName: 'flashMessage' }));
-    //app.use(flash());
+// Flash Messages
+app.use(flash({ sessionKeyName: 'flashMessage' }));
 
-
-
-//Templating Engine
+// Templating Engine
 app.use(expressLayout);
+app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
-app.set('layout', 'layouts/main');
 
-//Routes
+// Routes
 app.use('/', require('./server/routes/customer'))
 
-// //Home
-// app.get('/', (req, res) => {
-//     const locals ={
-//         title: 'NodeJs',
-//         description: 'Free NodeJs User Management System'
-//     }
-//     res.render('index', locals);
-// });
-
-//Handle 404
-app.get('*', (req,res) => {
-    res.status(404).render('404');
+// Handle 404
+app.get('*', (req, res) => {
+  res.status(404).render('404');
 });
 
-app.listen(port, ()=>{
-    console.log(`App listing in port ${port}`)
+app.listen(port, ()=> {
+  console.log(`App listeing on port ${port}`)
 });
